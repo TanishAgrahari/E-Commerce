3d7f9616f6b77588b3650c6ca20adda6538c34c0
TanishAgrahari <tanishagrahari2018@gmail.com>
2025-09-13 21:25:08 +0530

    Delete Frontend directory

--- Frontend/src/pages/Notfound/index.js
+++
@@ -1,9 +0,0 @@
-import Notfound from '../../component/notfound/Notfound';
-function Notfoundpage(){
-    return(
-        <>
-        <Notfound/>
-        </>
-    )
-}
-export default Notfoundpage;
